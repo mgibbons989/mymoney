9fdfc07853b4c6575279f3a932cc39672e574054
madison <mgibbons989@gmail.com>
2025-03-06 23:47:18 -0500

    added header, footer, and base payroll calcs

---
+++ my-money-react/src/components/footer.jsx
@@ -0,0 +1,12 @@
+
+
+function Footer() {
+    return (
+        <footer>
+            &copy; {new Date().getFullYear()} MyMoney Madison Gibbons,
+            Joe Capponi, Robert Shim, Liz
+        </footer>
+    )
+};
+
+export default Footer;
