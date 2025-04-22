6bfeccdf5d9b26b901764e42032ebde3205337c2
madison <mgibbons989@gmail.com>
2025-04-22 05:43:06 -0400

    implemented a calendar to the schedule page-- in progress

--- my-money-react/src/components/Schedule.jsx
+++ my-money-react/src/components/Schedule.jsx
@@ -1,7 +1,37 @@
 import { useState } from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Calendar from "./schedule/calendar";
+import Sidebar from "./Sidebar";
 
-const Schedule = () => {
+import "./schedule/sched.css"
 
+function Schedule() {
+    return (
+        <>
+            <Header />
+
+            <div className="main-content">
+
+                <div className="sched-content">
+
+                    <aside className="sidebar">
+                        <Sidebar />
+                    </aside>
+
+                    <div className="sched-main">
+                        <div>
+                            <Calendar />
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <Footer />
+        </>
+    )
 };
 
 export default Schedule;
