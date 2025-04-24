d4e3f9615ce559ca3a12d129529434c35e2a2cd4
Robert <robert.s.shim@gmail.com>
2025-04-24 01:38:51 -0400

    add shiftworkedlist.jsx

---
+++ my-money-react/src/components/payroll/shiftworkedlist.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+
+
+function ShiftWorkedList() {
+    const [shiftWorkedList, setShiftWorkedList] = useState([]);
+    useEffect(() => {
+        const token = localStorage.getItem("access_token");
+
+        const fetchData = async () => {
+            const resShiftWorkedList = await fetch("http://localhost:5000/getPayrolls", {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+            setShiftWorkedList(await resShiftWorkedList.json());
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            <span className="shift-header">Shifts</span>
+
+            <div className="card">
+                <ul className="shift-list">
+                    {shiftWorkedList.length === 0 ? (
+                        <li>No Payroll</li>
+                    ) : (
+                        shiftWorkedList.map(shift => (
+                            <li key={shift.id} className="shift">
+                                <span>[{shift.shift_date.slice(0,10)}] {shift.start_time} - {shift.end_time}</span>
+                                <span>{shift.hours.toFixed(2)}</span>
+                                <span>$ {shift.total_earned}</span>
+                            </li>
+                        ))
+                    )}
+                </ul>
+
+                <div className="total-wages">
+                    TOTAL WAGES:
+
+                    <span className="total-amt"> $TOTAL</span>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ShiftWorkedList;
